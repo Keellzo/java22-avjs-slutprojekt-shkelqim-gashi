76d6d29c9eb9feadc2fc039d3a851211040c098b
kelly gashi <kellygashi00@gmail.com>
2023-04-26 17:23:08 +0200

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import "../css/App.css";
+import { createRoot } from "react-dom/client";
+
+
+function App() {
+  return (
+    <div>
+     
+    </div>
+  );
+}
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
